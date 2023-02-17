6bfac037db502fa3bfd054c1c1704d452091bf05
Cristiano Silva <alchaarcristiano@gmail.com>
2023-02-17 22:42:31 +0000

    Fix background and color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,16 +7,11 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
-    /*:focus{
-        outline: 0;
-        box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
-    }*/
-
     body{
         min-width: 400px;
         overflow-x: hidden;
-        background: ${(props) => props.theme['gray-100']};
-        color: ${(props) => props.theme['gray-700']};
+        background: #FAFAFA;
+        color: #574F4D;
         -webkit-font-smoothing:antialiased; 
 
         @media screen and (max-width: 400px){
